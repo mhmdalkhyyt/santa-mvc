0796be8d843351ef8ee5164b8f733da593fc2a8f
a20muhal <102594578+a20muhal@users.noreply.github.com>
2022-10-23 16:18:56 +0200

    init push

---
+++ Santa-MVC/Controllers/ReindeerController.cs
@@ -0,0 +1,36 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Santa_MVC.Models;
+
+namespace Santa_MVC.Controllers
+{
+    public class ReindeerController : Controller
+    {
+        private readonly IConfiguration _configuration;
+        private ReindeerModel _reindeerModel { get; set; }
+
+        public ReindeerController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public IActionResult Index()
+        {
+            ReindeerModel reindeerModel = new ReindeerModel(_configuration);
+            ViewBag.ReindeerTable = reindeerModel.GetAllReindeers();
+            return View();
+        }
+
+        
+        public IActionResult InsertReindeer(int rNr, string rClanName, string rSubspecies, string rName, string rStink, string rRegion, int rGroupBellonging)
+        {
+            ReindeerModel reindeerModel = new ReindeerModel(_configuration);
+            reindeerModel.InsertReindeer(rNr, rClanName, rSubspecies, rName, rStink, rRegion, rGroupBellonging);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
+
+
---
+++ Santa-MVC/Models/ReindeerModel.cs
@@ -0,0 +1,62 @@
+﻿using System.Data;
+using MySql.Data.MySqlClient;
+
+
+
+namespace Santa_MVC.Models
+{
+    public class ReindeerModel
+    {
+        IConfiguration _configuration;
+        private string connectionString;
+
+        public ReindeerModel(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            connectionString = _configuration["ConnectionString"];
+        }
+
+        public DataTable GetAllReindeers()
+        {
+            MySqlConnection dbcon = new MySqlConnection(connectionString);
+            dbcon.Open();
+            MySqlDataAdapter adapter = new MySqlDataAdapter("SELECT * FROM Reindeer;", dbcon);
+            DataSet ds = new DataSet();
+            adapter.Fill(ds, "result");
+            DataTable reindeerTable = ds.Tables["result"];
+            dbcon.Close();
+
+            return reindeerTable;
+
+        }
+
+        public void InsertReindeer(int rNr, string rClanName, string rSubspecies, string rName, string rStink, string rRegion, int rGroupBellonging)
+        {
+            MySqlConnection dbcon = new MySqlConnection(connectionString);
+            dbcon.Open();
+
+            string insertQuery = "INSERT INTO Reindeer(Nr, ClanName, Subspecies, ReindeerName, Stink, Region, GroupBellonging)" +
+                "VALUES(@rNr, @rClanName, @rSubspecies, @rName, @rStink, @rRegion, @rGroupBellonging)";
+
+            MySqlCommand sqlCmd = new MySqlCommand(insertQuery, dbcon);
+            sqlCmd.Parameters.AddWithValue("@rNr", rNr);
+            sqlCmd.Parameters.AddWithValue("@rClanName", rClanName);
+            sqlCmd.Parameters.AddWithValue("@rSubspecies", rSubspecies);
+            sqlCmd.Parameters.AddWithValue("@rName", rName);
+            sqlCmd.Parameters.AddWithValue("@rStink", rStink);
+            sqlCmd.Parameters.AddWithValue("@rRegion", rRegion);
+            sqlCmd.Parameters.AddWithValue("@rGroupBellonging", rGroupBellonging);
+
+            int rows = sqlCmd.ExecuteNonQuery();
+            dbcon.Close();
+
+
+        }
+
+
+    }
+}
+
+
+
+
